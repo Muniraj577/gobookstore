0a96bb7ba1cbd233942cc98de6dd25b2efe1db80
Muniraj Rajbanshi <munirajrajbanshi@gmail.com>
2022-05-16 15:39:56 +0545

    change code

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,6 +1,6 @@
-package main 
+package main
 
-import (
+import(
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
@@ -12,5 +12,5 @@ func main(){
 	r := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(r)
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
