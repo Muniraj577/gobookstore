7564ee3bbb0e5daceff06407a1f6de03e78383a6
Muniraj577 <59973111+Muniraj577@users.noreply.github.com>
2022-05-16 18:45:20 +0545

    Update app.go

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -13,7 +13,7 @@ var (
 
 
 func Connect(){
-	d, err := gorm.Open("mysql", "root:@123@/gobookstore?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "root:@password/gobookstore?charset=utf8&parseTime=True&loc=Local")
 	if err != nil{
 		panic(err)
 	}
